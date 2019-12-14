03bc656dfbc71a6cb397b0519a1e708908f0674a
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-14 14:30:59 +0200

    created json tree based on file size

--- project-crawler/crawl.py
+++ project-crawler/crawl.py
@@ -5,6 +5,8 @@ import os
 
 from datetime import datetime
 from git.cmd import Git
+from anytree import Node, Resolver, RenderTree, LevelOrderIter
+from anytree.exporter import JsonExporter
 
 def print_stage(text):
     row_size=80
@@ -14,26 +16,57 @@ def print_stage(text):
     print(f"{'*'*row_size}");
 
 
-def parse_git_repository(root):
-    git = Git(root)
-    for f in git.ls_files().split():
-        folder = os.path.dirname(f) or '.'
+def parse_git_repository(path, output):
+    git = Git(path)
+    repository_name = os.path.basename(path)
+    print_stage(f'Analyzing {repository_name}')
+
+    root = Node(repository_name)
+    resolver = Resolver()
+    parent = root
+
+    for f in sorted(git.ls_files().split(), key=lambda s : s.count(os.sep)):
+        folder = os.path.dirname(f)
         file = os.path.basename(f)
-        print(folder, '/', file)
+        
+        if file.startswith('.'):
+            continue
+
+        if not folder:
+            parent = root
+        else:
+            try:
+                parent = resolver.get(root, f'{folder}')
+            except Exception as e:
+                # parent not found, we must create one
+                grandparent_folder = os.path.dirname(folder)
+                if not grandparent_folder:
+                    # root is the grandparent
+                    parent = Node(folder.split(os.sep)[-1], parent=root)
+                else:
+                    # here, the grandparent must exist in the tree
+                    grandparent = resolver.get(root, grandparent_folder)
+                    parent = Node(folder.split(os.sep)[-1], parent=grandparent)
+
+        file_size = os.path.getsize(f'{path}/{f}') >> 8
+        node = Node(file, parent=parent, value=file_size)
+
+    exporter = JsonExporter(indent=4)
+    with open(output, "wt") as out:
+        exporter.write(root, out)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--root', required=True, help='git repository root')
+    parser.add_argument('-o', '--output', required=True, help='output json file')
     args = parser.parse_args()
 
-    print_stage(f'Analyzing')
-    
     if not os.path.isdir(args.root):
         print(f'Error: the {args.root} is not a directory.')
         return -1
 
-    parse_git_repository(args.root)
+    parse_git_repository(args.root, output=args.output)
 
 
 if __name__ == '__main__':
