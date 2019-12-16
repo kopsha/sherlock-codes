f1fe9baec48821126d86b266081820f661a1dcf6
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-16 19:21:39 +0200

    working to identify match brackets

--- project-crawler/parse.py
+++ project-crawler/parse.py
@@ -1,7 +1,40 @@
+from datetime import datetime
 from utils import print_stage
+import timeit
+
+import pyparsing as pp
+
+def tick(t):
+    print(f'* {t}')
+
 
 def main():
-    print_stage("it works")
+    source_file = 'data/identify.cpp'
+    print_stage(f'Scanning {source_file}')
+
+
+    g = pp.Forward()
+
+    # nestedParens = nest('(', ')')
+    # nestedBrackets = nest('[', ']')
+    nestedCurlies = pp.nestedExpr('{', '}').setParseAction(tick)
+    #nest_grammar = nestedParens | nestedBrackets | nestedCurlies
+    nest_grammar = nestedCurlies
+
+    parens = "(){}[]"
+    letters = ''.join([x for x in pp.printables
+                       if x not in parens])
+    word = pp.Word(letters)
+
+    xxx = pp.OneOrMore(word | nest_grammar)
+    xxx.ignore(pp.cppStyleComment)
+    xxx.ignore(pp.dblQuotedString)
+
+    for token in xxx.parseFile(source_file):
+        print(len(token))
+
 
 if __name__ == '__main__':
-    main()
+    duration = timeit.timeit(main, number=1)
+    now = datetime.now().strftime('%H:%M:%S')
+    print_stage(f'[{now}] Finished in {duration:.2f} seconds.')
