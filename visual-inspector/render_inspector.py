2e54d37a1d36109f1c7e33626086920d212bcb4a
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-25 12:23:22 +0200

    added a radial dendro view

--- visual-inspector/render_inspector.py
+++ visual-inspector/render_inspector.py
@@ -9,18 +9,22 @@ from utils import print_stage
 import jinja2
 
 
-def render_inspector_page(data_folder, out_folder):
+def render_inspector_pages(data_folder, out_folder):
     print_stage('Rendering')
 
     datafiles = [f for f in os.listdir(data_folder) if f.endswith('.json')]
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
-    main_page = env.get_template('index.html')
-    output = os.path.join(out_folder, main_page.name)
+    pages = [
+        env.get_template('index.html'),
+        env.get_template('radial-dendo.html'),
+    ]
 
-    print(f'Writing {output}')
-    with open(output, "wt") as out:
-        out.write(main_page.render(datafiles=datafiles))
+    for page in pages:
+        output = os.path.join(out_folder, page.name)
+        print(f'Writing {output}')
+        with open(output, "wt") as out:
+            out.write(page.render(datafiles=datafiles))
 
     print('Copying data files')
     for f in datafiles:
@@ -41,7 +45,7 @@ def main():
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
-    render_inspector_page(args.data, args.output)
+    render_inspector_pages(args.data, args.output)
 
 
 if __name__ == '__main__':
