31b25324fb8b1c831dadcd52aaa4f6aa8208893a
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-26 17:36:56 +0200

    coupling diagram almost working with full path imports spec

--- visual-inspector/inspector-views.js
+++ visual-inspector/inspector-views.js
@@ -1,4 +1,5 @@
-function update_info_view(d) {
+function update_info_view(d)
+{
     info_text = "<p>"
     info_text += d.data.name + "<hr>";
 
@@ -27,7 +28,8 @@ function update_info_view(d) {
     info_view.html( info_text );
 }
 
-function create_svg_element(s) {
+function create_svg_element(s)
+{
     let div= document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
     div.innerHTML= '<svg xmlns="http://www.w3.org/2000/svg">'+s+'</svg>';
     let frag= document.createDocumentFragment();
@@ -36,7 +38,8 @@ function create_svg_element(s) {
     return frag;
 }
 
-function display_circlepack(data) {
+function display_circlepack(data)
+{
     let svg = d3.select("svg"),
         margin = 20,
         diameter = svg.attr("width"),
@@ -131,7 +134,8 @@ function display_circlepack(data) {
             });
     }
 
-    function zoomTo(v) {
+    function zoomTo(v)
+    {
         let k = diameter / v[2]; view = v;
         node.attr("transform", function(d) {
             return "translate("
@@ -143,7 +147,8 @@ function display_circlepack(data) {
     }
 }
 
-function display_radialdendro(data) {
+function display_radialdendro(data)
+{
     let svg = d3.select("svg").style("box-sizing", "border-box"),
         margin = 20,
         diameter = svg.attr("width"),
@@ -202,7 +207,113 @@ function display_radialdendro(data) {
         .attr("stroke", "white");
 }
 
-function render_architecture_view() {
+function display_couplings(data)
+{
+    function full_id(node)
+    {
+        return `${node.parent ? full_id(node.parent) + "/" : ""}${node.data.name}`;
+    }    
+    function bilink(root)
+    {
+        const map = new Map(root.leaves().map(d => [full_id(d), d]));
+        console.log(map)
+        for (const d of root.leaves())
+        {
+            d.incoming = [];
+            if (d.data.meta.imports)
+            {
+                for (const x of d.data.meta.imports.local)
+                {
+                    console.log(x, '--->', map.get(x) )
+                }
+                d.outgoing = d.data.meta.imports.local.map(i => [d, map.get(i)]);
+            }
+            else
+            {
+                d.outgoing = []
+            }
+        }
+
+        for (const d of root.leaves())
+        {
+            for (const o of d.outgoing)
+            {
+                o[1].incoming.push(o);    
+            }            
+        }  
+        return root;
+    }
+    function overed(d)
+    {
+        link.style("mix-blend-mode", null);
+        d3.select(this).attr("font-weight", "bold");
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", color_in).raise();
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", color_in).attr("font-weight", "bold");
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", color_out).raise();
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", color_out).attr("font-weight", "bold");
+    }
+
+    function outed(d)
+    {
+        link.style("mix-blend-mode", "multiply");
+        d3.select(this).attr("font-weight", null);
+        d3.selectAll(d.incoming.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.incoming.map(([d]) => d.text)).attr("fill", null).attr("font-weight", null);
+        d3.selectAll(d.outgoing.map(d => d.path)).attr("stroke", null);
+        d3.selectAll(d.outgoing.map(([, d]) => d.text)).attr("fill", null).attr("font-weight", null);
+    }
+
+    let svg = d3.select("svg");
+    let width = svg.attr("width");
+    let radius =  width / 2;
+    svg.attr("viewBox", [-width / 2, -width / 2, width, width]);
+
+    tree = d3.cluster()
+        .size([2 * Math.PI, radius - 100])
+
+    line = d3.lineRadial()
+        .curve(d3.curveBundle.beta(0.85))
+        .radius(d => d.y)
+        .angle(d => d.x);
+
+    let color_none = "#ccc";
+    let color_out = "#f00";
+    let color_in = "#00f";
+
+    const root = tree(bilink(d3.hierarchy(data)));
+    const node = svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll("g")
+        .data(root.leaves())
+        .join("g")
+            .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
+        .append("text")
+            .attr("dy", "0.31em")
+            .attr("x", d => d.x < Math.PI ? 6 : -6)
+            .attr("text-anchor", d => d.x < Math.PI ? "start" : "end")
+            .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
+            .text(d => d.data.name)
+            .each(function(d) { d.text = this; })
+            .on("mouseover", overed)
+            .on("mouseout", outed)
+            .call(text => text.append("title").text(d => `${full_id(d)}
+                ${d.outgoing.length} outgoing
+                ${d.incoming.length} incoming`));
+
+    const link = svg.append("g")
+        .attr("stroke", color_none)
+        .attr("fill", "none")
+        .selectAll("path")
+        .data(root.leaves().flatMap(leaf => leaf.outgoing))
+        .join("path")
+            .style("mix-blend-mode", "multiply")
+            .attr("d", ([i, o]) => line(i.path(o)))
+            .each(function(d) { d.path = this; });
+}
+
+function render_architecture_view()
+{
     let url = project_selector.val();
     let svg_tag = create_svg_element('<svg width="768" height="768">');
     graphic_view.empty();
@@ -213,14 +324,26 @@ function render_architecture_view() {
     });
 }
 
-function render_dependencies_view() {
+function render_dependencies_view()
+{
     let url = project_selector.val();
-    console.log(url, project_selector.val())
     let svg_tag = create_svg_element('<svg width="768" height="768">');
     graphic_view.empty();
     graphic_view.append(svg_tag);
     d3.json(url).then( function (data) {
         console.log("The promised display_radialdendro");
         display_radialdendro(data);
+    });
+}
+
+function render_coupling_view()
+{
+    let url = project_selector.val();
+    let svg_tag = create_svg_element('<svg width="768" height="768">');
+    graphic_view.empty();
+    graphic_view.append(svg_tag);
+    d3.json(url).then( function (data) {
+        console.log("The promised display_couplings");
+        display_couplings(data);
     });
 }
