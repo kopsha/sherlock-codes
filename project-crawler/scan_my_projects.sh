55424e025a814974272a1a8d2bf2b83f6c6ca4c0
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-28 19:19:02 +0200

    added parsing all local files

---
+++ project-crawler/scan_my_projects.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+python crawl.py -r ~/src/entr
+python crawl.py -r ~/src/optimal-transaction-hunter
+python crawl.py -r ~/src/sherlock-codes
+
+python crawl.py -r ~/wrk/alber/bloks
+python crawl.py -r ~/wrk/alber/gui
+python crawl.py -r ~/wrk/alber/gui-lcd
+
+python crawl.py -r ~/wrk/turbo-connect-core
+python crawl.py -r ~/wrk/turbo-connect-android-lib
+python crawl.py -r ~/wrk/turbo-connect-ios-lib
+python crawl.py -r ~/wrk/mission-control
+
+mv *.json ../visual-inspector/data
