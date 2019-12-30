8bd28876656eb8975a25e99e005ecb07a285ad08
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2019-12-30 22:02:35 +0200

    heatmap completed, could work on corners and padding

--- visual-inspector/heatmap-view.js
+++ visual-inspector/heatmap-view.js
@@ -1,3 +1,89 @@
+function natural_split(text)
+{
+    const caseChanged = (string) => /[a-z][A-Z]/.test(string)
+
+    parts = []
+    points = []
+    last = 0
+
+    for (i = 1; i < text.length; i++)
+    {
+        if (caseChanged(text.slice(i-1,i+1)))
+        {
+            points.push(i-last)
+            last = i
+        }
+        else if (/[-_\.]/.test(text.slice(i,i+1))) {
+            points.push(i-last)
+            last = i
+        }
+    }
+    points.push(text.length)
+
+    while (pos = points.shift())
+    {
+        parts.push(text.slice(0, pos))
+        text = text.slice(pos)
+    }
+
+    return parts
+}
+
+function smart_text_fit(text_tags)
+{
+    const padding = 8
+    text_tags.each(function () {
+        let text = d3.select(this);
+        let content = text.text()
+        let cx = parseFloat(text.attr("x"));
+        let cy = parseFloat(text.attr("y"));
+        let width = parseFloat(text.attr("box_width"));
+        let height = parseFloat(text.attr("box_height"));
+
+        if (height > width)
+        {
+            text.attr("transform", `rotate(-90, ${cx}, ${cy})`);
+            [width, height] = [height, width]
+        }
+
+        if (text.node().getComputedTextLength() >= (width - padding)
+            || (height < 20))
+        {
+            text.classed("small-label", false)
+                .classed("xsmall-label", true);
+
+            if (text.node().getComputedTextLength() >= (width - padding))
+            {
+                lines = natural_split(content)
+                line_height = 7;
+                if (lines.length*line_height < (height - padding))
+                {
+                    half = lines.length/2;
+                    top_dy = -line_height*half + 2.9;
+                    bottom_dy = line_height*half + 2.9;
+                    line_ys = d3.scaleLinear()
+                        .domain([0, lines.length-1])
+                        .range([top_dy, bottom_dy]);
+
+                    text.text(null);
+                    lines.forEach( function(line,i) {
+                        text.append("tspan")
+                            .attr("x", cx)
+                            .attr("y", cy)
+                            .attr("dy", line_ys(i))
+                            .text(line);
+                    });
+                }
+                // again, if it does not fit, give up
+                if (text.node().getComputedTextLength() >= (width - padding))
+                {
+                    text.text("···");  // last resort
+                }
+            }
+        }
+    });
+}
+
 function render_heatmap(data)
 {
     var margin = {
@@ -8,7 +94,6 @@ function render_heatmap(data)
         }
     let svg = d3.select("svg"),
         g = svg.append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     let width = svg.attr("width");
     let height = svg.attr("height");
 
@@ -19,7 +104,7 @@ function render_heatmap(data)
     let thermal_scale = Array.from(thermal_set).sort((a,b) => b-a)
     let legend_box_height = height / thermal_scale.length
     let color = d3.scaleSequential(d3.interpolateTurbo)
-        .domain([-3, thermal_scale[0] * 1.25]);    // Leave off the edges of the scale
+        .domain([-0.125*thermal_scale[0], 1.25*thermal_scale[0]]);  // Leave off the edges of the scale
 
     d3.select("#info_view").selectAll("*").remove()
     let legend_info = d3.select("#info_view").style("text-align", "left")
@@ -41,13 +126,15 @@ function render_heatmap(data)
         .data(thermal_scale)
         .enter()
             .append('text')
-            .attr("class", "small-label")
+            .attr("class", "xsmall-label")
             .attr('x', 40)
             .attr('y', (d,i) => (legend_box_height/2) + i*legend_box_height)
+            .attr('dy', 2.9)
             .text(function (d) {return `${d} °`})
 
     d3.treemap()
         .size([width, height])
+        .tile(d3.treemapResquarify)
         .padding(1)
         (root);
 
@@ -55,38 +142,25 @@ function render_heatmap(data)
         .data(root.leaves())
         .enter()
         .append("rect")
-            .style("fill", function(d) {
-                return color((d.data.temperature) ? d.data.temperature : 1);
-            })
-            .attr('x', function(d) {
-                return d.x0;
-            })
-            .attr('y', function(d) {
-                return d.y0;
-            })
-            .attr('width', function(d) {
-                return d.x1 - d.x0;
-            })
-            .attr('height', function(d) {
-                return d.y1 - d.y0;
-            });
+            .style("fill", d => color(d.data.temperature))
+            .attr('x', d => d.x0)
+            .attr('y', d => d.y0)
+            .attr('width', d => d.x1-d.x0)
+            .attr('height', d => d.y1-d.y0);
 
     svg
         .selectAll("text")
         .data(root.leaves())
         .enter()
         .append("text")
-        .attr("class", "small-label")
-        .attr("x", function(d) { return d.x0 + (d.x1-d.x0)/2 })
-        .attr("y", function(d) { return d.y0 + (d.y1-d.y0)/2 })
-        .text(function(d) { return d.data.name })
-        .attr("transform", function (d) {
-            let w = d.x1 - d.x0;
-            let h = d.y1 - d.y0;
-            let x = d.x0 + w/2 
-            let y = d.y0 + h/2 
-            return (h > w) ? `rotate(-90, ${x}, ${y})` : `rotate(0, ${x}, ${y})`;
-        });
+            .classed("small-label", true)
+            .attr("x", d => d.x0 + (d.x1-d.x0)/2)
+            .attr("y", d => 2 + d.y0 + (d.y1-d.y0)/2)
+            .attr('dy', 2)
+            .attr("box_width",  d => d.x1 - d.x0)
+            .attr("box_height", d => d.y1 - d.y0)
+            .text(d => d.data.name)
+        .call(smart_text_fit);
 }
 
 function render_heatmap_view()
